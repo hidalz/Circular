47a3c704eef5c5957f43b92853cc146e88caea13
hidalz <m.hidalglez@gmail.com>
2020-12-11 11:14:45 +0900

    Added beta version

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  return <ViewImageScreen />;
+}
